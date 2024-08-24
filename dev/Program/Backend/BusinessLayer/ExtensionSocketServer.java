bf9ceda12ffb2c098efac763b43763427a866968
bhaa225 <117650059+bhaa225@users.noreply.github.com>
2024-08-24 22:53:53 +0300

    adding libs

--- dev/Program/Backend/BusinessLayer/ExtensionSocketServer.java
+++ dev/Program/Backend/BusinessLayer/ExtensionSocketServer.java
@@ -12,4 +12,45 @@ import java.util.Scanner;
 
 public class ExtensionSocketServer extends WebSocketServer{
 
+    private List<ChromeWindow> googleWindows;
+    private Thread workerThread;
+    
+
+    public ExtensionSocketServer(int port){
+        super(new InetSocketAddress(port));
+    }
+
+    @Override
+    public void onOpen(WebSocket conn, ClientHandshake handshake) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'onOpen'");
+    }
+
+    @Override
+    public void onClose(WebSocket conn, int code, String reason, boolean remote) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'onClose'");
+    }
+
+    @Override
+    public void onMessage(WebSocket conn, String message) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'onMessage'");
+    }
+
+    @Override
+    public void onError(WebSocket conn, Exception ex) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'onError'");
+    }
+
+    @Override
+    public void onStart() {
+        
+    }
+
+    public static void main(String [] args){
+        ExtensionSocketServer s = new ExtensionSocketServer(8887);
+        s.start();
+    }
 }
